15dcaba6828a7f1b11cfeebb937315a16486cd13
Denlz Polat <91384872+DenizzPolatt@users.noreply.github.com>
2023-01-15 01:22:20 +0300

    Implement Enemy Shooting Mechanism

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private GameObject enemyShotPrefab;
-
-    private float timer = 3;
+    private float timer;
+    private float timer2;
+    private float gameBegun;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer += Random.Range(3, 40);
     }
 
     // Update is called once per frame
@@ -22,8 +23,7 @@ public class EnemyController : MonoBehaviour
 
     private void ShootPlayer()
     {
-        float randomTime = Random.Range(1, 5);
-
+        float randomTime = Random.Range(5, 30);
         if (timer < 0)
         {
             timer += randomTime;
@@ -31,4 +31,5 @@ public class EnemyController : MonoBehaviour
         }
 
     }
+    
 }
